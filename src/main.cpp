b02127a21d11c8c7ae8ab9fc28ba91dacee25a34
Spitfire <Spitfire@SpitfireXP.de>
2024-10-26 14:04:19 +0200

    add Relay

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 //====================================== Temperatureinstellungen
-#define solltemp 20
+#define solltemp 22
 #define stufe1 solltemp+1
 #define stufe2 solltemp+2
 #define stufe3 solltemp+3
@@ -13,6 +13,7 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 
 #define DHTPIN 4
 #define FanPin 5
+#define RelayPin 6
 #define DHTTYPE DHT11
 float temp;
 String status;
@@ -25,17 +26,21 @@ lcd.backlight();
 dht.begin();
 //TCCR1B = TCCR1B & B11111000 | B00000001;
 pinMode(FanPin,OUTPUT);
+pinMode(RelayPin,OUTPUT);
 }
 void loop() {
 delay(2000);
 float temp = dht.readTemperature();
-if (temp <= solltemp) { digitalWrite(FanPin,LOW); status = "AUS      "; }else
-if (temp >= stufe5) { digitalWrite(FanPin,HIGH); status = "Stufe-MAX"; }else
-if (temp >= stufe4) { analogWrite(FanPin,190); status = "Stufe-4  "; }else
-if (temp >= stufe3) { analogWrite(FanPin,150); status = "Stufe-3  "; }else
+if (temp <= solltemp) { digitalWrite(RelayPin,LOW); status = "AUS      "; }else
+    { digitalWrite(RelayPin,HIGH);}
+
+if (temp >= stufe5) { digitalWrite(FanPin,LOW); status = "Stufe-MAX"; }else
+if (temp >= stufe4) { analogWrite(FanPin,40); status = "Stufe-4  "; }else
+if (temp >= stufe3) { analogWrite(FanPin,60); status = "Stufe-3  "; }else
 if (temp >= stufe2) { analogWrite(FanPin,90); status = "Stufe-2  "; }else
-if (temp >= stufe1) { analogWrite(FanPin,60); status = "Stufe-1  "; }else
-if (temp >= solltemp) { analogWrite(FanPin,40); status = "Stufe-LOW"; }
+if (temp >= stufe1) { analogWrite(FanPin,150); status = "Stufe-1  "; }else
+if (temp >= solltemp) { analogWrite(FanPin,220); status = "Stufe-LOW"; }
+
 lcd.setCursor(0,0);
 lcd.print("L\365ftersteuerung");
 lcd.setCursor(0,1);
